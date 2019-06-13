e2b147aa4c0d40e72276001e156f66d6c94542f9
Bogdan Perian <bogdan.perian@fxtorial.com>
2019-06-13 10:37:46 -0400

    make sure api token is set; health check service

--- arcanna/app.py
+++ arcanna/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask import jsonify
 from elasticsearch import Elasticsearch
 from lib.tf import TFNode
@@ -12,6 +12,22 @@ app.config.from_envvar('CONFIG_PATH')
 tf_node = TFNode()
 
 
+@app.route('/health', methods=['POST'])
+def health_check():
+        token = request.values.get('api_token')
+        if token is None or token != current_app.config("API_TOKEN"):
+                return abort(400)
+        response = {
+                "status":"ok"
+        }
+        return jsonify(response)
+
+
 # Main entry point for application
 if __name__ == '__main__':
-    app.run(debug=True,host=app.config['BIND_IP'],port=app.config['BIND_PORT'])
+	if "API_TOKEN" not in app.config.keys() or app.config["API_TOKEN"] == "":
+		print(" - you need to set API_TOKEN entry in the configuration")
+		sys.exit(0)
+
+	app.run(debug=True,host=app.config['BIND_IP'],port=app.config['BIND_PORT'])
+	
